2903cc1a09e23a0bf83e578c32da57e958e5ad14
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:40:58 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-registration - Trademark registration toolkit - DPMA, EUIPO, WIPO filing automation. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
